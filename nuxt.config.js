c885fb6edd80389bd02194efffddd4893261a5eb
mirexdoors <mirex.doors@mail.ru>
2019-11-13 01:56:44 +0700

    eslint tunes

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: 'Рейтинг федерации сквоша России' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -36,7 +36,7 @@ export default {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify'
   ],
   /*
@@ -62,7 +62,7 @@ export default {
     theme: {
       dark: true,
       themes: {
-        dark: {
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
